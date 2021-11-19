459afb019305080b535f81fb122411a31900bf14
Luan Vieira <lvieira@invoca.com>
2021-11-19 10:17:43 -0800

    initial commit

---
+++ lib/kubo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kubo/version"
+
+module Kubo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kubo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kubo do
+  it "has a version number" do
+    expect(Kubo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
